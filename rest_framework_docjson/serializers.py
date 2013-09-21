18b0e96c9708bc8fc97d22d8e9f13f3e202b651e
mike <michael@iminhaus.com>
2013-09-20 21:48:11 -0400

    make DocJSONModelSerializer work when used as a nested field

--- rest_framework_docjson/serializers.py
+++ rest_framework_docjson/serializers.py
@@ -4,7 +4,21 @@ from rest_framework import serializers
 class DocJSONModelSerializer(serializers.ModelSerializer):
     @property
     def data(self):
+
         data = super(DocJSONModelSerializer, self).data
         if self.many:
             return {'_type': 'list', 'items': [item for item in data]}
+        return data
+
+    def field_to_native(self, obj, field_name):
+        """
+        wraps the nested representation in the DocJSON list type
+        """
+
+        data = super(DocJSONModelSerializer, self)\
+            .field_to_native(obj, field_name)
+
+        if hasattr(data, '__iter__'):
+            return {'_type': 'list', 'items': [item for item in data]}
+
         return data
