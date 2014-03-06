23e6403502c7e44d77bd7b762cae7dff6c3a7336
Michael <mdpaulus@gmail.com>
2014-03-05 23:06:51 -0500

    change pagination links to be normal docjson link elements

--- rest_framework_docjson/pagination.py
+++ rest_framework_docjson/pagination.py
@@ -1,17 +1,36 @@
 from rest_framework import pagination
 
 
+class DocJSONPaginationFieldMixin(object):
+
+	def to_native(self, value):
+		url = super(DocJSONPaginationFieldMixin, self).to_native(value)
+		if url:
+			return {
+				'_type': 'link',
+				'url': url
+			}
+		return
+
+
+class DocJSONNextPageField(DocJSONPaginationFieldMixin, pagination.NextPageField):
+	pass
+
+class DocJSONPreviousPageField(DocJSONPaginationFieldMixin, pagination.PreviousPageField):
+	pass
+
+
 class DocJSONPaginationSerializer(pagination.BasePaginationSerializer):
     """
-    The minimum DocJSON requirement for list pagination
+    wraps the 'Next' page url in the DocJSON link format
     """
-    next = pagination.NextPageField(source='*')
+    next = DocJSONNextPageField(source='*')
     results_field = 'items'
 
 
 
 class DocJSONPaginationSerializerWithPrevious(DocJSONPaginationSerializer):
     """
-    DocJSON list pagination with a 'previous' property
+    wraps the 'Next' and 'Previous' page url in the DocJSON link format
     """
-    previous = pagination.PreviousPageField(source='*')
+    previous = DocJSONPreviousPageField(source='*')
